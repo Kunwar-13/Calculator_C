9b1b83675e7b736336ebd04122a501c8ca549643
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2024-11-15 04:18:00 -0500

    Basic Framework

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+
+	printf("Choose your operatoration: \n");
+	printf("1. Addition(+)\n");
+	printf("2. Subtraction(-)\n");
+	printf("3. Multiplication(x)\n");
+	printf("4. Division(/)\n");
+	printf("5. Square(\u00B2)\n");
+	printf("6. Cube(\u00B3)\n");
+	printf("7. Square root(\u221A)\n");
+	printf("8. Square root(\u221B)\n");
+
+}
