5a14d6e2714f9a91b3775eb9b3c1b5319dd73903
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2024-11-18 18:24:59 -0500

    Segregated th code

---
+++ Functions.cpp
@@ -0,0 +1,31 @@
+double addition(double array[], int x) {
+
+	int i = 0;
+	double sum = 0;
+
+	for (i = 0; i < x; i++) {
+
+		sum += array[i];
+
+	}
+
+	return sum;
+}
+
+
+
+double subtraction(double array[], int x) {
+	double sum = 0.00;
+	int y = 0;
+	while (y < 1) {
+		ans = num;
+		y++;
+	}
+	for (int i = 0; i < x; i++) {
+
+		sum -= array[i];
+	}
+
+	return sum;
+
+}
--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,3 @@
-#include<stdio.h>
-#include<string.h>
-
-double addition(double[], int);
 
 int main(void) {
 
@@ -38,34 +34,3 @@ int main(void) {
 	}
 }
 
-double addition(double array[], int x) {
-
-	int i = 0;
-	double sum = 0;
-
-	for (i = 0; i < x; i++) {
-
-		sum += array[i];
-
-	}
-
-	return sum;
-}
-
-
-
-double subtraction(double array[], int x) {
-	double sum = 0.00;
-	int y = 0;
-	while (y < 1) {
-		ans = num;
-		y++;
-	}
-	for (int i = 0; i < x; i++) {
-
-		sum -= array[i];
-	}
-
-	return sum;
-
-}
