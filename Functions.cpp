dec6bd6a2630471b97c94b035cc4a33447bdac48
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2024-11-18 18:42:03 -0500

    Subtraction editited

--- Functions.cpp
+++ Functions.cpp
@@ -1,7 +1,7 @@
 double addition(double array[], int x) {
 
 	int i = 0;
-	double sum = 0;
+	double answer = 0;
 
 	for (i = 0; i < x; i++) {
 
@@ -15,15 +15,22 @@ double addition(double array[], int x) {
 
 
 double subtraction(double array[], int x) {
-	double sum = 0.00;
+	double answer = 0.00;
 	int y = 0;
-	while (y < 1) {
-		ans = num;
-		y++;
-	}
+	
 	for (int i = 0; i < x; i++) {
 
-		sum -= array[i];
+		if (i == 0) {
+
+			answer = array[i];
+
+		}
+		else {
+
+			sum -= array[i];
+
+		}
+		
 	}
 
 	return sum;
